4cf1aa402c1d193919358f524d2c2f6f8f55fe71
wang.tianyou <wangtianyou@xiaoma.cn>
2017-03-29 09:55:43 +0800

    Initial Commit

---
+++ ProtoBufferDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProtoBufferDemo
+//
+//  Created by WTY on 17/3/29.
+//  Copyright © 2017年 WTY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProtoBufferDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtoBufferDemo
+//
+//  Created by WTY on 17/3/29.
+//  Copyright © 2017年 WTY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
